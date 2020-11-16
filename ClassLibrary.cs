000c5b709af30ef4e39076c6d9e2a560fcefff6e
Sanya <sanyahladunyk@gmail.com>
2020-11-16 14:36:49 +0200

    Добавьте файлы проекта.

---
+++ ClassLibrary.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_lab7
+{
+    public class Human
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirthday { get; set; }
+        public Human()
+        {
+            Name = "Undefined";
+            Surname = "Undefined";
+            DateOfBirthday = new DateTime(1, 1, 1);
+        }
+        public Human(string name, string surname, DateTime dateOfBirthday)
+        {
+            Name = name;
+            Surname = surname;
+            DateOfBirthday = new DateTime(dateOfBirthday.Year, dateOfBirthday.Month, dateOfBirthday.Day);
+        }
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine(Name + ' ' + Surname + "\n" +
+                DateOfBirthday.Day + '.' + DateOfBirthday.Month + '.' + DateOfBirthday.Year);
+        }
+    }
+    public class Entrant : Human
+    {
+        public double PointsZNO { get; set; }
+        public double PointsForADocumentAboutEducation { get; set; }
+        public string SecondarySchool { get; set; }
+        public Entrant()
+        {
+            Name = "Undefined";
+            Surname = "Undefined";
+            DateOfBirthday = new DateTime(1, 1, 1);
+            PointsZNO = 0;
+            PointsForADocumentAboutEducation = 0;
+            SecondarySchool = "Undefined";
+        }
+        public Entrant(string name, string surname, DateTime dateOfBirthday, double pointsZNO, double pointsForADocumentAboutEducation, string secondarySchool)
+        {
+            Name = name;
+            Surname = surname;
+            DateOfBirthday = new DateTime(dateOfBirthday.Year, dateOfBirthday.Month, dateOfBirthday.Day);
+            PointsZNO = pointsZNO;
+            PointsForADocumentAboutEducation = pointsForADocumentAboutEducation;
+            SecondarySchool = secondarySchool;
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Абітурієнт - {Name} {Surname}\n" +
+                $"Дата народження - {DateOfBirthday.Day}.{DateOfBirthday.Month}.{DateOfBirthday.Year}\n" +
+                $"Кількість балів сертифікатів ЗНО - {PointsZNO}\n" +
+                $"Кількість балів за документ про освіту - {PointsForADocumentAboutEducation}\n" +
+                $"Назва загально-освітнього закладу - {SecondarySchool}");
+        }
+    }
+    public class Student:Human
+    {
+        public int Cours { get; set; }
+        public string Grup { get; set; }
+        public string Faculty { get; set; }
+        public string InstitutionOfHigherEducation { get; set; }
+        public Student()
+        {
+            Name = "Undefined";
+            Surname = "Undefined";
+            DateOfBirthday = new DateTime(1, 1, 1);
+            Cours = 0;
+            Grup = "Undefined";
+            Faculty = "Undefined";
+            InstitutionOfHigherEducation = "Undefined";
+        }
+        public Student(string name, string surname, DateTime dateOfBirthday, int cours, string grup, string faculty, string institutionOfHigherEducation)
+        {
+            Name = name;
+            Surname = surname;
+            DateOfBirthday = new DateTime(dateOfBirthday.Year, dateOfBirthday.Month, dateOfBirthday.Day);
+            Cours = cours;
+            Grup = grup;
+            Faculty = faculty;
+            InstitutionOfHigherEducation = institutionOfHigherEducation;
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Студент - {Name} {Surname}\n" +
+                $"Дата народження - {DateOfBirthday.Day}.{DateOfBirthday.Month}.{DateOfBirthday.Year}\n" +
+                $"Курс - {Cours}\n" +
+                $"Група - {Grup}\n" +
+                $"Факультет - {Faculty}\n" +
+                $"Вищий навчальний заклад - {InstitutionOfHigherEducation}");
+        }
+    }
+    public class Teacher:Human
+    {
+        public string Position { get; set; }
+        public string Cathedra { get; set; }
+        public string InstitutionOfHigherEducation { get; set; }
+        public Teacher()
+        {
+            Name = "Undefined";
+            Surname = "Undefined";
+            DateOfBirthday = new DateTime(1, 1, 1);
+            Position = "Undefined";
+            Cathedra = "Undefined";
+            InstitutionOfHigherEducation = "Undefined";
+        }
+        public Teacher(string name, string surname, DateTime dateOfBirthday, string position, string cathedra, string institutionOfHigherEducation)
+        {
+            Name = name;
+            Surname = surname;
+            DateOfBirthday = new DateTime(dateOfBirthday.Year, dateOfBirthday.Month, dateOfBirthday.Day);
+            Position = position;
+            Cathedra = cathedra;
+            InstitutionOfHigherEducation = institutionOfHigherEducation;
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Викладач - {Name} {Surname}\n" +
+                $"Дата народження - {DateOfBirthday.Day}.{DateOfBirthday.Month}.{DateOfBirthday.Year}\n" +
+                $"Посада - {Position}\n" +
+                $"Кафедра - {Cathedra}\n" +
+                $"Вищий навчальний заклад - {InstitutionOfHigherEducation}");
+        }
+    }
+    public class LibraryUser:Human
+    {
+        public int ReadersTicketNumber { get; set; }
+        public DateTime DateOfIssue { get; set; }
+        public double MonthlyAmountReadersFee { get; set; }
+        public LibraryUser()
+        {
+            Name = "Undefined";
+            Surname = "Undefined";
+            DateOfBirthday = new DateTime(1, 1, 1);
+            ReadersTicketNumber = 0;
+            DateOfIssue = new DateTime(1, 1, 1);
+            MonthlyAmountReadersFee = 0;
+        }
+        public LibraryUser(string name, string surname, DateTime dateOfBirthday, int readersTicketNumber, DateTime dateOfIssue, double monthlyAmountReadersFee)
+        {
+            Name = name;
+            Surname = surname;
+            DateOfBirthday = new DateTime(dateOfBirthday.Year, dateOfBirthday.Month, dateOfBirthday.Day);
+            ReadersTicketNumber = readersTicketNumber;
+            DateOfIssue = new DateTime(dateOfIssue.Year, dateOfIssue.Month, dateOfIssue.Day);
+            MonthlyAmountReadersFee = monthlyAmountReadersFee;
+        }
+        public override void ShowInfo()
+        {
+            Console.WriteLine($"Користувач бібліотеки - {Name} {Surname}\n" +
+                $"Дата народження - {DateOfBirthday.Day}.{DateOfBirthday.Month}.{DateOfBirthday.Year}\n" +
+                $"Номер читацького квитка - {ReadersTicketNumber}\n" +
+                $"Дата видачі - {DateOfIssue.Day}.{DateOfIssue.Month}.{DateOfIssue.Year}\n" +
+                $"Розмір щомісячного читацького внеску - {MonthlyAmountReadersFee}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace oop_lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Entrant> Entrant = new List<Entrant>();
+            List<Student> Student = new List<Student>();
+            List<Teacher> Teacher = new List<Teacher>();
+            List<LibraryUser> LibraryUser = new List<LibraryUser>();
+            Entrant.Add(new Entrant("Олександр", "Гладуник", new DateTime(2002, 2, 17), 120, 10.2, "Турківська ЗОШ"));
+            Entrant.Add(new Entrant());
+            Student.Add(new Student());
+            Teacher.Add(new Teacher());
+            LibraryUser.Add(new LibraryUser());
+            while(true)
+            {
+                Console.Write("\n\n1. Переглянути масив абітурієнтів\n" +
+                    "2. Переглянути масив студентів\n" +
+                    "3. Переглянути масив викладачів\n" +
+                    "4. Переглянути масив користувачів бібліотеки\n" +
+                    "5. Додати абітурєнта\n" +
+                    "6. Додати студента\n" +
+                    "7. Додати викладача\n" +
+                    "8. Додати користувача бібліотеки\n" +
+                    "9. Вихід\n" +
+                    ">>>");
+                if (!Int32.TryParse(Console.ReadLine(), out int n) || n > 10 || n < 0) 
+                    Console.WriteLine("\n     Введено некоректне значення! Повторіть спробу.");
+                else if(n==1)
+                    foreach (var item in Entrant)
+                    {
+                        Console.WriteLine("\n");
+                        item.ShowInfo();
+                    }
+                else if (n == 2)
+                    foreach (var item in Student)
+                    {
+                        Console.WriteLine("\n");
+                        item.ShowInfo();
+                    }
+                else if (n == 3)
+                    foreach (var item in Teacher)
+                    {
+                        Console.WriteLine("\n");
+                        item.ShowInfo();
+                    }
+                else if (n == 4)
+                    foreach (var item in LibraryUser)
+                    {
+                        Console.WriteLine("\n");
+                        item.ShowInfo();
+                    }
+                else if (n == 5)
+                {
+                    string Name,
+                        Surname,
+                        SecondarySchool;
+                    double PointsZNO, 
+                        PointsEducation;
+                    SetHuman(out Name, out Surname, out int Day, out int Month, out int Year);
+                    if (Day == 0 || Day > 31)
+                        Day = 1;
+                    if (Month == 0 || Month > 12)
+                        Month = 1;
+                    if (Year == 0)
+                        Year = 1;
+                    Console.Write("Кількість балів сертифікатів ЗНО: ");
+                    Double.TryParse(Console.ReadLine(), out PointsZNO);
+                    Console.Write("Кількість балів за документ про освіту: ");
+                    Double.TryParse(Console.ReadLine(), out PointsEducation);
+                    Console.Write("Назва загальноосвітнього навчального закладу: ");
+                    SecondarySchool = Console.ReadLine();
+                    Entrant.Add(new Entrant(Name, Surname, new DateTime(Year, Month, Day), PointsZNO, PointsEducation, SecondarySchool));
+                }
+                else if (n == 6)
+                {
+                    string Name,
+                        Surname,
+                        Grup,
+                        Faculty,
+                        InstitutionOfHigherEducation;
+                    int Cours;
+                    SetHuman(out Name, out Surname, out int Day, out int Month, out int Year);
+                    if (Day == 0 || Day > 31)
+                        Day = 1;
+                    if (Month == 0 || Month > 12)
+                        Month = 1;
+                    if (Year == 0)
+                        Year = 1;
+                    Console.Write("Курс: ");
+                    Int32.TryParse(Console.ReadLine(), out Cours);
+                    Console.Write("Група: ");
+                    Grup = Console.ReadLine();
+                    Console.Write("Факультет: ");
+                    Faculty = Console.ReadLine(); 
+                    Console.Write("Вищий навчальний заклад: ");
+                    InstitutionOfHigherEducation = Console.ReadLine();
+                    Student.Add(new Student(Name, Surname, new DateTime(Year, Month, Day), Cours, Grup, Faculty, InstitutionOfHigherEducation));
+                }
+                else if (n == 7)
+                {
+                    string Name,
+                        Surname,
+                        Position,
+                        Cathedra,
+                        InstitutionOfHigherEducation;
+                    SetHuman(out Name, out Surname, out int Day, out int Month, out int Year);
+                    if (Day == 0 || Day > 31)
+                        Day = 1;
+                    if (Month == 0 || Month > 12)
+                        Month = 1;
+                    if (Year == 0)
+                        Year = 1;
+                    Console.Write("Посада: ");
+                    Position = Console.ReadLine();
+                    Console.Write("Кафедра: ");
+                    Cathedra = Console.ReadLine();
+                    Console.Write("Вищий навчальний заклад: ");
+                    InstitutionOfHigherEducation = Console.ReadLine();
+                    Teacher.Add(new Teacher(Name, Surname, new DateTime(Year, Month, Day), Position, Cathedra, InstitutionOfHigherEducation));
+                }
+                else if (n == 8)
+                {
+                    string Name,
+                        Surname;
+                    int ReadersTicketNumber;
+                    double MonthlyAmountReadersFee;
+                    SetHuman(out Name, out Surname, out int Day, out int Month, out int Year);
+                    if (Day == 0 || Day > 31) 
+                        Day = 1;
+                    if (Month == 0 || Month > 12) 
+                        Month = 1;
+                    if (Year == 0)
+                        Year = 1;
+                    Console.Write("Номер читацького квитка: ");
+                    Int32.TryParse(Console.ReadLine(), out ReadersTicketNumber);
+                    Console.Write(" Дата видачі\nДень: ");
+                    Int32.TryParse(Console.ReadLine(), out int IssueDay);
+                    Console.Write("Місяць: ");
+                    Int32.TryParse(Console.ReadLine(), out int IssueMonth);
+                    Console.Write("Рік: ");
+                    Int32.TryParse(Console.ReadLine(), out int IssueYear);
+                    Console.Write("Розмір щомісячного читацького внеску: ");
+                    Double.TryParse(Console.ReadLine(), out MonthlyAmountReadersFee);
+                    if (IssueDay == 0 || IssueDay > 31)
+                        IssueDay = 1;
+                    if (IssueMonth == 0 || IssueMonth > 12)
+                        IssueMonth = 1;
+                    if (IssueYear == 0)
+                        IssueYear = 1;
+                    LibraryUser.Add(new LibraryUser(Name, Surname, new DateTime(Year, Month, Year), ReadersTicketNumber, new DateTime(IssueYear, IssueMonth, IssueDay), MonthlyAmountReadersFee));
+                }
+                else if (n == 9)
+                    break;
+            }
+            void SetHuman(out string name, out string surname, out int day, out int month, out int year)
+            {
+                Console.Write("Ім'я: ");
+                name = Console.ReadLine();
+                Console.Write("Прізвище: ");
+                surname = Console.ReadLine();
+                Console.Write(" Дата народження\nДень: ");
+                Int32.TryParse(Console.ReadLine(), out day);
+                Console.Write("Місяць: ");
+                Int32.TryParse(Console.ReadLine(), out month);
+                Console.Write("Рік: ");
+                Int32.TryParse(Console.ReadLine(), out year);
+
+            }
+        }
+    }
+}
